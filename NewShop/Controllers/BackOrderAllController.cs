8722ad3eb6ce3246880a59374f9f9b5963756dbc
armoop <armoop03@gmail.com>
2024-02-21 13:53:06 +0700

    [bin&view&controller]backorder update

--- NewShop/Controllers/BackOrderAllController.cs
+++ NewShop/Controllers/BackOrderAllController.cs
@@ -28,7 +28,7 @@ namespace NewShop.Controllers
             }
             return View();
         }
-        public ActionResult Filter(string CUSCOD, string SLMID, string Show_Flag, string Stock_Flag, string stkcod, string DocumentNo,string COM)
+        public ActionResult Filter(string CUSCOD, string SLMID, string Show_Flag, string Stock_Flag, string stkcod, string DocumentNo, string COM, string UserType)
         {
 
             int intFlag = Convert.ToInt32(Show_Flag);
@@ -65,6 +65,7 @@ namespace NewShop.Controllers
             command.Parameters.AddWithValue("@DocumentNo", DocumentNo);
             command.Parameters.AddWithValue("@Stock_Flag ", intstock);
             command.Parameters.AddWithValue("@Com", COM);
+            command.Parameters.AddWithValue("@UsrTyp", UserType);
             command.ExecuteNonQuery();
             SqlDataReader drb = command.ExecuteReader();
             //1=by Doc,2=by Item
@@ -179,7 +180,7 @@ namespace NewShop.Controllers
             }
         }
 
-        public JsonResult getbackorder(string CUSCOD, string SLMID, string Show_Flag, string Stock_Flag, string stkcod, string DocumentNo, string COM)
+        public JsonResult getbackorder(string CUSCOD, string SLMID, string Show_Flag, string Stock_Flag, string stkcod, string DocumentNo, string COM, string UserType)
         {
 
             int intFlag = Convert.ToInt32(Show_Flag);
@@ -207,7 +208,7 @@ namespace NewShop.Controllers
             BackOrderbyDoc _modelDoc = new BackOrderbyDoc();
 
             var command = new SqlCommand("P_Search_BackOrder", Connection);
-           
+
             command.CommandType = CommandType.StoredProcedure;
             command.Parameters.AddWithValue("@SaleCode", SLMID);
             command.Parameters.AddWithValue("@Show_Flag", intFlag);
@@ -216,6 +217,7 @@ namespace NewShop.Controllers
             command.Parameters.AddWithValue("@DocumentNo", DocumentNo);
             command.Parameters.AddWithValue("@Stock_Flag ", intstock);
             command.Parameters.AddWithValue("@Com", COM);
+            command.Parameters.AddWithValue("@UsrTyp", UserType);
             command.ExecuteNonQuery();
             SqlDataReader drb = command.ExecuteReader();
             //1=by Doc,2=by Item
@@ -312,7 +314,7 @@ namespace NewShop.Controllers
                         KDCQty = drb["KDCQty"].ToString(),
                         PDCQty = drb["PDCQty"].ToString(),
                         Rating = drb["Rating"].ToString(),
-                       
+
                     });
 
                 }
@@ -336,7 +338,7 @@ namespace NewShop.Controllers
             return Json(GetdatabyItem, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult InsertdataBackorder(string Cuspo ,string  User, string   CUSCOD,string   DocNo,string  SeqNo ,string VSTKCOD ,string MyIndexValueQty,string MyIndexValueQtyclear,string MyIndexValueQtyLeftover,string company)
+        public JsonResult InsertdataBackorder(string Cuspo, string User, string CUSCOD, string DocNo, string SeqNo, string VSTKCOD, string MyIndexValueQty, string MyIndexValueQtyclear, string MyIndexValueQtyLeftover, string company)
         {
 
             string message = "false";
@@ -347,46 +349,46 @@ namespace NewShop.Controllers
             try
             {
 
-                    using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MobileOrder_ConnectionString"].ConnectionString))
-                    {
-                        connection.Open();
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MobileOrder_ConnectionString"].ConnectionString))
+                {
+                    connection.Open();
 
-                        SqlCommand command = new SqlCommand("P_Save_BackOrder_catalog", connection);
-                            command.CommandType = CommandType.StoredProcedure;
-                            //command.Parameters.AddWithValue("@InBckNo", ValueBo);
-                            command.Parameters.AddWithValue("@inCUSCOD", CUSCOD);
-                            command.Parameters.AddWithValue("@inDocNo", DocNo);
-                            command.Parameters.AddWithValue("@inSeqNo", SeqNo);
-                            command.Parameters.AddWithValue("@inSTKCOD", VSTKCOD);
-                            command.Parameters.AddWithValue("@inqty", MyIndexValueQty);
-                            command.Parameters.AddWithValue("@inClearqty", MyIndexValueQtyclear);
-                            command.Parameters.AddWithValue("@inRemqty", MyIndexValueQtyLeftover);
-                            command.Parameters.AddWithValue("@Com", company);
-                            command.Parameters.AddWithValue("@User ", User);
-                            command.Parameters.AddWithValue("@Cuspo", Cuspo);
-                            SqlParameter returnValue = new SqlParameter("@outResult", SqlDbType.NVarChar, 100);
-                            returnValue.Direction = System.Data.ParameterDirection.Output;
-                            command.Parameters.Add(returnValue);
+                    SqlCommand command = new SqlCommand("P_Save_BackOrder_catalog", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    //command.Parameters.AddWithValue("@InBckNo", ValueBo);
+                    command.Parameters.AddWithValue("@inCUSCOD", CUSCOD);
+                    command.Parameters.AddWithValue("@inDocNo", DocNo);
+                    command.Parameters.AddWithValue("@inSeqNo", SeqNo);
+                    command.Parameters.AddWithValue("@inSTKCOD", VSTKCOD);
+                    command.Parameters.AddWithValue("@inqty", MyIndexValueQty);
+                    command.Parameters.AddWithValue("@inClearqty", MyIndexValueQtyclear);
+                    command.Parameters.AddWithValue("@inRemqty", MyIndexValueQtyLeftover);
+                    command.Parameters.AddWithValue("@Com", company);
+                    command.Parameters.AddWithValue("@User ", User);
+                    command.Parameters.AddWithValue("@Cuspo", Cuspo);
+                    SqlParameter returnValue = new SqlParameter("@outResult", SqlDbType.NVarChar, 100);
+                    returnValue.Direction = System.Data.ParameterDirection.Output;
+                    command.Parameters.Add(returnValue);
 
 
-                            command.ExecuteNonQuery();
-                            //S20161016
-                            command.Dispose();
-                            //E20161016
+                    command.ExecuteNonQuery();
+                    //S20161016
+                    command.Dispose();
+                    //E20161016
 
-                            message = returnValue.Value.ToString();
-                            connection.Close();
-                        }
+                    message = returnValue.Value.ToString();
+                    connection.Close();
+                }
 
-                        //S20161016
-                       
-                        //E20161016
-                      //connection.Close();
-                    
+                //S20161016
 
-                   // connection.Close();
+                //E20161016
+                //connection.Close();
 
-               
+
+                // connection.Close();
+
+
             }
             catch (Exception ex)
             {
