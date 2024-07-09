077940776b72f79fa2727b36720635e4c5e072bd
armoop <armoop03@gmail.com>
2024-07-09 15:07:19 +0700

    [bin&controller&view]  freeze amt and edit timeout command

--- NewShop/Controllers/CustomerDashboardController.cs
+++ NewShop/Controllers/CustomerDashboardController.cs
@@ -368,6 +368,7 @@ namespace NewShop.Controllers
             {
                 var command = new SqlCommand("p_Order_Notify_Detail", Connection);
                 command.CommandType = CommandType.StoredProcedure;
+                command.CommandTimeout = 120;
                 command.Parameters.AddWithValue("@CUSCOD", CUSCOD.Trim());
                 command.Parameters.AddWithValue("@NotifyID", NotifyID);
                 SqlDataReader reader = command.ExecuteReader();
