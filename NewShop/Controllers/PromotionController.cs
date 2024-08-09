d59fba4c25824215ffe5d59fa695ba1d7c7a47c2
warakornaac <warakorn.pra@aac.co.th>
2024-08-09 09:21:22 +0700

    [controlle&view] add page sales register promotion

--- NewShop/Controllers/PromotionController.cs
+++ NewShop/Controllers/PromotionController.cs
@@ -27,8 +27,31 @@ namespace NewShop.Controllers
         public ActionResult RegisterCustomer()
         {
             string user = Session["UserID"].ToString();
+            string slmCodeDefault = Session["UserID"].ToString();
+            string flagSup = string.Empty;
+
+            using (SqlConnection Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MobileOrder_ConnectionString"].ConnectionString))
+            {
+                Connection.Open();
+                if (slmCodeDefault != null)
+                {
+                    SqlCommand cmd1 = new SqlCommand("select TOP 1 * From v_SLMTAB_SM_Userrid where SUP = N'" + slmCodeDefault + "'", Connection);
+                    SqlDataReader rev = cmd1.ExecuteReader();
+                    while (rev.Read())
+                    {
+                        flagSup = rev["SUP"].ToString();
+                    }
+                    rev.Close();
+                    rev.Dispose();
+                    cmd1.Dispose();
+                }
+            }
+
             ViewBag.slmCodeList = GetSalesmanName(user);
-            ViewBag.pmCodeList = GetProductName(user);
+            ViewBag.pmCodeList = GetProductName("");
+            //ViewBag.slmCode = slmCode == null ? slmCodeDefault : slmCode;
+            ViewBag.flagSup = flagSup;
+
             return View();
         }
         //get name sales
@@ -58,18 +81,17 @@ namespace NewShop.Controllers
 
             return slmCodeList;
         }
-        public List<SelectListItem> GetProductName(string pmCode)
+        public List<SelectListItem> GetProductName(string User)
         {
             List<SelectListItem> productList = new List<SelectListItem>();
             var connectionString = ConfigurationManager.ConnectionStrings["MobileOrder_ConnectionString"].ConnectionString;
             SqlConnection Connection = new SqlConnection(connectionString);
             Connection.Open();
 
-            var  command = new SqlCommand("P_Price_Approve_Data", Connection);
+            var command = new SqlCommand("P_Search_Product", Connection);
             command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("@inUsrID", pmCode);
-            command.Parameters.AddWithValue("@inType", 4);
-            //command.ExecuteNonQuery();
+            command.Parameters.AddWithValue("@inUser", User);
+            command.ExecuteNonQuery();
             SqlDataReader dr = command.ExecuteReader();
             while (dr.Read())
             {
@@ -84,6 +106,219 @@ namespace NewShop.Controllers
             Connection.Close();
 
             return productList;
+        }
+        //get PromotionCode By Pmcode
+        public JsonResult GetPromotion(string prodMgr, string year, string company)
+        {
+            List<listPromotionCode> promotionList = new List<listPromotionCode>();
+            SqlConnection Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Promotion_ConnectionString"].ConnectionString);
+            Connection.Open();
+            var command = new SqlCommand("P_Search_Promotioncode", Connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@inPm", prodMgr);
+            command.Parameters.AddWithValue("@inYear", year);
+            command.Parameters.AddWithValue("@inCom", company);
+            SqlDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                promotionList.Add(new listPromotionCode()
+                {
+                    PromotionCode = dr["Promotion_code"].ToString(),
+                    PromotionDes = dr["Description"].ToString()
+                });
+            }
+            dr.Close();
+            dr.Dispose();
+            command.Dispose();
+            Connection.Close();
+            return Json(promotionList, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetPromotionDetail(string promotionCode)
+        {
+            string message = "";
+            List<listPromotionDetail> promotionDetailList = new List<listPromotionDetail>();
+            var connectionString = ConfigurationManager.ConnectionStrings["Promotion_ConnectionString"].ConnectionString;
+            SqlConnection Connection = new SqlConnection(connectionString);
+            Connection.Open();
+            try
+            {
+                var command = new SqlCommand("P_Search_Promotion_Detail", Connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@inPromotionCode", promotionCode);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    promotionDetailList.Add(new listPromotionDetail()
+                    {
+                        Promotion_Code = reader["Promotion_Code"].ToString(),
+                        Seq = reader["Seq"].ToString(),
+                        Type = reader["Type"].ToString(),
+                        DesType = reader["DesType"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        Condition = reader["Condition"].ToString(),
+                        Reward = reader["Reward"].ToString(),
+                        Reward_Percent = reader["Reward_Percent"].ToString()
+                    });
+                }
+                //if (Getdata.Any())
+                //{
+                //    //get data WH, Round, PIDate, StartDelivery, ExptoArrive
+                //    foreach (var rowData in Getdata)
+                //    {
+                //    }
+                //}
+                @ViewBag.promotionDetailList = promotionDetailList;
+                reader.Close();
+                command.Dispose();
+                Connection.Close();
+                message = "Y";
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            @ViewBag.messageError = message;
+            return PartialView("_DetailPromotionRegister", new
+            {
+                @ViewBag.promotionDetailList,
+                @ViewBag.messageError
+            });
+        }
+        public ActionResult GetCustomerByPromotion(string slmCode, string promotionCode, string promotionSeq, string textHeader)
+        {
+            int countCustomer = 0;
+            int countCustomerReg = 0;
+            string message = "Y";
+            List<listCustomerRegister> customerRegisterList = new List<listCustomerRegister>();
+            var connectionString = ConfigurationManager.ConnectionStrings["Promotion_ConnectionString"].ConnectionString;
+            SqlConnection Connection = new SqlConnection(connectionString);
+            Connection.Open();
+            try
+            {
+                var command = new SqlCommand("P_Search_Customer_Register", Connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@inSlmCode", slmCode);
+                command.Parameters.AddWithValue("@inPromotionCode", promotionCode);
+                command.Parameters.AddWithValue("@inPromotionSeq", promotionSeq);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ++countCustomer;
+                    if (!string.IsNullOrEmpty(reader["FLAG_REG"].ToString())) {
+                        ++countCustomerReg;
+                    }
+                    customerRegisterList.Add(new listCustomerRegister()
+                    {
+                        Cuscode = reader["CUSCOD"].ToString(),
+                        Cusname = reader["CUSNAM"].ToString(),
+                        Slmcode = reader["SLMCOD"].ToString(),
+                        FlagReg = reader["FLAG_REG"].ToString(),
+                        FlagApprove = reader["FLAG_APPROVE"].ToString()
+                    });
+                }
+                @ViewBag.customerRegisterList = customerRegisterList;
+                reader.Close();
+                command.Dispose();
+                Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            @ViewBag.messageError = message;
+            @ViewBag.countCustomer = countCustomer;
+            @ViewBag.countCustomerReg = countCustomerReg;
+            @ViewBag.textHeader = textHeader;
+            return PartialView("_ListCustomer", new
+            {
+                @ViewBag.customerRegisterList,
+                @ViewBag.messageError,
+                @ViewBag.textHeader,
+                @ViewBag.countCustomer,
+                @ViewBag.countCustomerReg
+            });
+        }
+        [HttpPost]
+        public ActionResult SaveCustomerRegister(string user, string slmCode, string promotionCode, string promotionSeq, string[] cusCode)
+        {
+            int numSuccess = 0;
+            int numError = 0;
+            string message = "Y";
+            string cusCodArr = "";
+            if (cusCode != null) { 
+                cusCodArr = String.Join(",", cusCode.Select(s => "" + s + ""));
+            }
+            var connectionString = ConfigurationManager.ConnectionStrings["Promotion_ConnectionString"].ConnectionString;
+            SqlConnection Connection = new SqlConnection(connectionString);
+            try
+            {
+                Connection.Open();
+                //foreach (var listData in (List<listCustomerRegisterSave>)request)
+                //{
+                    var command = new SqlCommand("P_Save_Customer_Register", Connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@inUser", user);
+                    command.Parameters.AddWithValue("@inSlmCode", slmCode);
+                    command.Parameters.AddWithValue("@inPromotionCode", promotionCode);
+                    command.Parameters.AddWithValue("@inPromotionSeq", Convert.ToInt32(promotionSeq));
+                    command.Parameters.AddWithValue("@inCustomerCode", cusCodArr);
+                    SqlParameter returnValue = new SqlParameter("@outGenstatus", SqlDbType.NVarChar, 100);
+                    returnValue.Direction = System.Data.ParameterDirection.Output;
+                    command.Parameters.Add(returnValue);
+                    command.ExecuteNonQuery();
+                    message = returnValue.Value.ToString();
+                    //if (message == "Y")
+                    //{
+                    //    ++numSuccess;
+                    //}
+                    //else
+                    //{
+                    //    ++numError;
+                    //}
+                    command.Dispose();
+                //}
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return Json(new { status = message, numSuccess = numSuccess, numError = numError }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult ApproveChangeCustomer()
+        {
+            string user = Session["UserID"].ToString();
+            string flagSup = string.Empty;
+
+            List<SelectListItem> slmCodeList = new List<SelectListItem>();
+
+            using (SqlConnection Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MobileOrder_ConnectionString"].ConnectionString))
+            {
+                Connection.Open();
+                //get salesman
+                var command = new SqlCommand("P_Price_Approve_Data", Connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@inUsrID", user);
+                command.Parameters.AddWithValue("@inType", 2);
+                SqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    slmCodeList.Add(new SelectListItem() { Value = dr["SLMCOD"].ToString(), Text = dr["SLMCOD"].ToString() + "/" + dr["SLMNAM"].ToString() });
+                }
+                dr.Close();
+                command.Dispose();
+                command.Dispose();
+            }
+
+            ViewBag.slmCodeList = slmCodeList;
+            ViewBag.pmCodeList = GetProductName(user);
+            //ViewBag.slmCode = slmCode == null ? slmCodeDefault : slmCode;
+            ViewBag.flagSup = flagSup;
+
+            return View();
         }
     }
 }
