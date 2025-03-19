c860f4dc8bad477e9977d92df2d4ea246d07bd5b
ArmAAC <armoop03@gmail.com>
2025-03-19 10:49:34 +0700

    [bin&controller&models&view] add page for show data STKCOD order on Mobile

--- NewShop/Models/Groupdata.cs
+++ NewShop/Models/Groupdata.cs
@@ -1313,4 +1313,20 @@ namespace NewShop.Models
         public string Username { get; set; }
         public string CusName { get; set; }
     }
+
+    public class StkCanSale
+    {
+        public string WH { get; set; }
+        public string Company { get; set; }
+        public string STKCOD { get; set; }
+        public string STKDES { get; set; }
+        public string STKGRP { get; set; }
+        public string Cansale_Qty { get; set; }
+        public string UOM { get; set; }
+        public string ReadyQty { get; set; }
+        public string MO_Qty { get; set; }
+        public string SO_Qty { get; set; }
+        public string InStock { get; set; }
+        public string BckDue { get; set; }
+    }
 }
