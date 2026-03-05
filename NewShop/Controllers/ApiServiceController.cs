301b27ad5b272c36b9f5c1e85a919e9e475817f9
ArmAAC <armoop03@gmail.com>
2026-03-05 17:17:18 +0700

    [controller&model] get line id & call api send flex message

--- NewShop/Controllers/ApiServiceController.cs
+++ NewShop/Controllers/ApiServiceController.cs
@@ -65,5 +65,47 @@ namespace NewShop.Controllers
             }
         }
 
+        public async Task<string> SendNotificateLine(string Uid, string Toppic, string ToppicCo, string stkcod, string stkdes, string Price, string Qty, string cuscod, string cusnam, string apprvby, string apprvdat, string user)
+        {
+            var urlAPI = "https://localhost:44361/Post/PushMessageSale";
+            var post = new ListPMNotificate
+            {
+                Uid = Uid,
+                Topic = Toppic,
+                Topiccod = ToppicCo,
+                stkcod = stkcod,
+                stkdes = stkdes,
+                SPrice = Price,
+                Qty = Qty,
+                Cuscod = cuscod,
+                cusnam = cusnam,
+                ApprvBy = apprvby,
+                ApprvDate = apprvdat,
+                user = user
+            }
+            try
+            {
+                var handler = new HttpClientHandler();
+                ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+
+                var client = new HttpClient(handler);
+                string jsonContent = JsonConvert.SerializeObject(post);
+                HttpContent content = new StringContent(jsonContent, Encoder.UTF8, "application/json");
+                HttpResponeMessage respone = await client.PostAsync(urlAPI, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    return responseContent;
+                }
+                else
+                {
+                    return response.StatusCode.ToString();
+                }
+            }
+            catch (Exception ex) { return ex.Message; }
+
+        }
+
     }
 }
